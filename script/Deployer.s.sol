4d1c71186cf30dee2a46c49b2e14c230e53d9f69
SamkitSoni <samkitsoni09@gmail.com>
2025-04-15 12:52:05 +0000

    Added Bash File

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -20,11 +20,26 @@ contract TokenDeployer is Script {
         pool = new RebaseTokenPool(
             IERC20(address(token)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress
         );
-        token.grantMintAndBurnRole(address(pool));
+        vm.stopBroadcast();
+    }
+}
+
+contract SetPermission is Script {
+    function grantRole(address token, address pool) public {
+        vm.startBroadcast();
+        IRebaseToken(token).grantMintAndBurnRole(address(pool));
+        vm.stopBroadcast();
+    }
+
+    function setAdmin(address token, address pool) public {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.startBroadcast();
         RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustomAddress).registerAdminViaOwner(address(token));
         TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(token));
         TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).setPool(address(token), address(pool));
         vm.stopBroadcast();
+
     }
 }
 
